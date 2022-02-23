fd63d39a9b42e066d975989f1c6c6e2dbb316e84
Shashikumar <shashirgowdas67@gmail.com>
2022-02-23 12:39:19 +0530

    Added:print maximum using generics

--- Find Maximum Number/MaximumNumberCheck.cs
+++ Find Maximum Number/MaximumNumberCheck.cs
@@ -22,5 +22,21 @@ namespace MaximumNumberGenerics
             }
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
     }
 }
--- Find Maximum Number/Program.cs
+++ Find Maximum Number/Program.cs
@@ -10,6 +10,10 @@ namespace MaximumNumberGenerics
             Console.WriteLine("Find maximum Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 44);
             Console.WriteLine(output);
+
+            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(12.7, 20.47, 80.56);
+            Console.WriteLine(doubleoutput);
+
         }
     }
 }
