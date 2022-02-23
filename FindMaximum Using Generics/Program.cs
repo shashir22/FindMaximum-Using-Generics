f3883c9b7840ec3c20d16e1afb0e7894285a3fe5
Shashikumar <shashirgowdas67@gmail.com>
2022-02-23 12:34:07 +0530

    Added:To find maximum integers

---
+++ FindMaximum Using Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MaximumNumberGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to generics program");
+            Console.WriteLine("Find maximum Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 44);
+            Console.WriteLine(output);
+        }
+    }
+}
