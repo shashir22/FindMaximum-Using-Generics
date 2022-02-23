9c549b83d810e8b26f9be3740b61c09b6988de03
Shashikumar <shashirgowdas67@gmail.com>
2022-02-23 13:01:51 +0530

    Added:Checking maximum using generics

--- Find Maximum Number/Program.cs
+++ Find Maximum Number/Program.cs
@@ -7,15 +7,28 @@ namespace MaximumNumberGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics program");
-            Console.WriteLine("Find maximum Number");
-            int output = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 44);
-            Console.WriteLine(output);
+            //Console.WriteLine("Find maximum Number");
+            //int output = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 44);
+            //Console.WriteLine(output);
 
-            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(12.7, 20.47, 80.56);
-            Console.WriteLine(doubleoutput);
+            //double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(12.7, 20.47, 80.56);
+            //Console.WriteLine(doubleoutput);
 
-            string strigoutput = MaximumNumberCheck.MaximumStringNumber("111", "222", "333");
-            Console.WriteLine(strigoutput);
+            //string strigoutput = MaximumNumberCheck.MaximumStringNumber("111", "222", "333");
+            //Console.WriteLine(strigoutput);
+
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+
+
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+
+
+            string[] stringArray = { "apple", "peach", "Banana", "Grapes" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+
+            Console.ReadKey();
 
         }
     }
